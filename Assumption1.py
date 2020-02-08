bc1d6fd8322b1e000e5e0d056678648910f39636
ekawitjaidee <ekawit_jaidee@cmu.ac.th>
2020-02-08 22:24:20 +0700

    association

--- Assumption1.py
+++ Assumption1.py
@@ -26,19 +26,50 @@ def random1():
 def association(list1):
     countTran = []
     a = []
+    listitem = []
+    listcount = [0]*len(list1)
+    sup = 0
+    t = []
     for i in range(len(list1)):
-        print(list1[i][1],len(list1[i][1]))
-        for j in range(len(list1[i][1])):
-            print(list1[i][1][j])
-            for k in range(len(item)):
-                if item[k] == list1[i][1][j]:
-                    a.append(list1[i][0])
-                countTran.append(item[k],a)
-                a = []
+        t.append(i)
+    # for i in range(len(list1)):
+    #     # print(list1[i][1],len(list1[i][1]))
+    #     for j in range(len(list1[i][1])):
+    #         # print(list1[i][1][j])
+    #         for k in range(len(item)):
+    #             if item[k] == list1[i][1][j]:
+    #                 a.append(list1[i][0])
+    #                 print(a)
+    #     countTran.append(item[k],a)
+    #     a = []
+    for i in range(len(item)):
+        for j in range(len(list1)):
+            for k in range(len(list1[j][1])):
+                # print(len(list1[1][1]))
+                if item[i] == list1[j][1][k]:
+                    # print(list1[j][1][k])
+                    a.append(list1[j][0])
+                    listcount[j] = 1
+                    sup = sup + 1
+                    # print(a)
+        listitem.append(("ITEM" + str(i+1) , a,listcount,sup))
+        listcount = [0]*len(list1)
+        a=[]
+        sup = 0
+    # print(listitem)  
+    print("Item            transaction ID            Support")
+    print("       "+str(t))
+    for i in range(len(item)):
+        if i < 9:
+            print(str(listitem[i][0])+"  "+str(listitem[i][2])+"       "+str(listitem[i][3]/len(list1))) 
+        else:
+            print(str(listitem[i][0])+" "+str(listitem[i][2])+"       "+str(listitem[i][3]/len(list1))) 
         
+               
 
-            
+
 
 
 # main
 association(random1())
+
