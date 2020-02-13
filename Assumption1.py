3ddbcd7f4c5cdaec2adccc2a65b620a28acd8ff8
jnpat <35354784+jnpat@users.noreply.github.com>
2020-02-13 16:43:14 +0700

    complete

--- Assumption1.py
+++ Assumption1.py
@@ -2,9 +2,10 @@ import random
 
 itemSize = 50
 item = []
-minSup = 3
+minSup = 2
 answer = []
 check = 0
+once = False
 
 for i in range(itemSize):
     item.append("Item" + str(i+1))
@@ -22,14 +23,8 @@ def random1():
             a.append(("T" + str(i),random.randint(6,8)))
 
         transList.append(("T" + str(i) ,random.sample(item, a[i][1])))
-
-    f = open("result1.txt","w+")
-
-    for i in range(len(transList)):
-        f.write(str(transList[i]) + "\n")
-
-    f.close()
-    print(str(transList) + "\n")   
+    
+    print1(transList)
     return transList
 
 def findSup(list1):
@@ -45,20 +40,12 @@ def findSup(list1):
                     countTran[j] = 1
                     sup += 1
         itemname.append("Item" + str(i+1))
-        listItem.append((itemname,countTran,float(sup)/len(list1)*100))
+        listItem.append((itemname,countTran,float(sup)/len(list1)*100,1))
         itemname = []
         countTran = [0]*len(list1)
         sup = 0
 
-    # print
-    # print(str(listItem)+"\n")
-    # print("Item             transaction ID           Support")
-    # for i in range(len(item)):
-    #     if i < 9:
-    #         print(str(listItem[i][0])+"    "+str(listItem[i][1])+"     "+str(listItem[i][2])) 
-    #     else:
-    #         print(str(listItem[i][0])+"   "+str(listItem[i][1])+"     "+str(listItem[i][2]) + "\n")
-
+    print1(listItem)
     return listItem
     
 def findFreqItem(list1):
@@ -68,15 +55,12 @@ def findFreqItem(list1):
         if list1[i][2] >= minSup:
             answer.append(list1[i])
             freqItem.append(list1[i])
+            
     if len(freqItem) == 0:
         print("no item support more than minsup")
         return freqItem
         
-    # # print
-    # print("Frequency Item Set = " + str(len(list1[0][0])))
-    # print("Item                 transaction ID           Support")
-    # for i in range(len(freqItem)):
-    #     print(str(freqItem[i][0])+"   "+str(freqItem[i][1])+"     "+str(freqItem[i][2]))
+    print1(freqItem)
     
     return freqItem
 
@@ -87,6 +71,7 @@ def matching(list1):
     itemSet = []
     listreturn = []
     sup = 0
+    keepsup = 0 
     for i in range(len(list1)):
         j = i+1
         while(j<len(list1)):
@@ -96,37 +81,30 @@ def matching(list1):
                     sup += 1
                     if len(list1[i][0]) and len(list1[j][0]) == 1:
                         itemSet = list1[i][0] + list1[j][0]
+                        keepsup = list1[i][2]
                     else:
-                        # print("----")
                         for l in range(len(list1[i][0])):
                             for m in range(len(list1[j][0])):
                               if list1[i][0][l] == list1[j][0][m]:
-                                # print(list1[i][0])
+                                keepsup = list1[i][2]
                                 itemSet = list1[i][0]+list1[j][0]
                                 listduplicate(itemSet)
                                 
             if len(itemSet) > 0:
-                listreturn.append((itemSet,listmatch,float(sup)/len(list1[i][1])*100))
+                listreturn.append((itemSet,listmatch,float(sup)/len(list1[i][1])*100,keepsup))
             itemSet = []
             sup = 0
             j=j+1
             listmatch = [0]*len(list1[0][1])
-    
-    # if len(listreturn)>0:
-    #     print("jame")
-    #     print("Frequency Item Set = " + str(len(listreturn[0][0])))
-    #     print("Item                                 transaction ID           Support")
-    #     for i in range(len(listreturn)):
-    #         print(str(listreturn[i][0])+"   "+str(listreturn[i][1])+"     "+str(listreturn[i][2]))
+    print1(listreturn)
 
     while len(listreturn) > 1:
-        # print("jean")
         return matching(findFreqItem(listreturn))
     if len(listreturn) == 1: 
         return listreturn
     else:
         return 1
-            
+    
 def listduplicate(list1):
     for i in range(len(list1)):
         j=i+1
@@ -136,7 +114,22 @@ def listduplicate(list1):
             j += 1
                 
     return list1
-        
+
+def print1(list1):
+    f = open("result1.txt","a")
+    # print(len(list1))
+    if len(list1[0]) ==2:
+        f.write("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n")
+        for i in range(len(list1)):
+            f.write(str(list1[i]) + "\n")
+    else:
+        f.write("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n")
+        for j in range(len(list1)):
+            f.write("Item Set = " + str(list1[j][0]) +  " Transaction = " + str(len(list1[j][1]))
+            + " Support = " + str(list1[j][2]) + " SupportPair = " + str(list1[j][3]) + "\n")
+    f.close()  
+
+
 # main
 ans = random1()
 ans = findSup(ans)
@@ -145,10 +138,13 @@ ans = matching(ans)
 # print("ans is")
 # # print(ans)
 # print("-------------------------------------------")
-# print(answer)
+
 f = open("result.txt","w+")
 
 for i in range(len(answer)):
-    f.write("Item Set = " + str(answer[i][0]) + "       Support = " + str(answer[i][2])+ "        Size = " + str(len(answer[i][0])) + "\n")
+    if len(answer[i][0])==1:
+        f.write("Item Set = " + str(answer[i][0]) + "       Support = " + str(answer[i][2])+ "        Size = " + str(len(answer[i][0])) +"\n")
+    else:
+        f.write("Item Set = " + str(answer[i][0]) + "       Support = " + str(answer[i][2])+ "        Size = " + str(len(answer[i][0])) + "       conf = "+str(answer[i][2]/answer[i][3]*100) + "\n")
 
 f.close()
